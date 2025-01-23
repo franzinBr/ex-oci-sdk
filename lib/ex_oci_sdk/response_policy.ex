6ca0fa39418d951fcf415091b4b64f7ea5678ddc
franzinBr <alan.franzin2011@gmail.com>
2025-01-23 19:26:01 -0400

    update data struct from status code sucess policy

--- lib/ex_oci_sdk/response_policy.ex
+++ lib/ex_oci_sdk/response_policy.ex
@@ -2,9 +2,9 @@ defmodule ExOciSdk.ResponsePolicy do
   @moduledoc """
   Defines policies for handling HTTP responses
   """
-  defstruct status_codes_success: [200]
+  defstruct status_codes_success: 200..299
 
   @type t :: %__MODULE__{
-          status_codes_success: [integer()]
+          status_codes_success: list(integer()) | Range.t()
         }
 end
